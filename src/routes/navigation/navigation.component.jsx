93d3b026b1538f648af2d0b525afb963b8d77c43
aspicy <acharyaakshit@gmail.com>
2024-01-16 23:33:27 +0530

    added routing using react-router-dom and a navigation bar

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import './navigation.styles.scss';
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom"
+
+import CrownLogo from '../../assets/crown.svg' // Can Use vite-svg-loader to import svg as react component
+
+const Navigation = () => {
+    return (
+      <Fragment>
+        <div className="navigation">
+          <Link className="logo-container" to='/'>
+            <img src={CrownLogo} alt='Logo' className='logo' />
+          </Link>
+          <div className="nav-links-container">
+            <Link className="nav-link" to='/shop'>
+                SHOP
+            </Link>
+          </div>
+        </div>
+        <Outlet />
+      </Fragment>
+    )
+}
+
+export default Navigation;
