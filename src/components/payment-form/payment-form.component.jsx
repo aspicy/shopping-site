c8d080430e7ea5db07549c60a59e5dccb5e561fc
aspicy <acharyaakshit@gmail.com>
2024-01-30 21:53:52 +0530

    commented out a console log in payment-form component

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -32,7 +32,7 @@ const PaymentForm = () => {
             body: JSON.stringify({ amount: amount * 100 })
         }).then(res => res.json());
 
-        console.log(response);
+        // console.log(response);
 
         const {paymentIntent: { client_secret }} = response;
 
