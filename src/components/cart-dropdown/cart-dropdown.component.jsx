1974a96e209be3fc8f2a9579d9fbda11bdb6791a
aspicy <acharyaakshit@gmail.com>
2024-01-31 07:26:02 +0530

    Go to checkout button now closes cart dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,18 +1,21 @@
 import {CartDropdownContainer, EmptyMessage, CartItems} from "./cart-dropdown.styles";
 
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { selectCartItems } from "../../store/cart/cart.selector";
+import { setIsCartOpen } from "../../store/cart/cart.action";
 
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
 
 const CartDropdown = () => {
+    const dispatch = useDispatch();
     const cartItems = useSelector(selectCartItems);
     const navigate = useNavigate();
 
     const goToCheckoutHandler = () => {
         navigate('/checkout');
+        dispatch(setIsCartOpen(false));
     }
 
     return (
